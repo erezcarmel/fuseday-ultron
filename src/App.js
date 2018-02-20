1ed5b7ba40ad1c0854fedc2d4f0b55be56c6e897
Ofer Ziss <oferzi@Ofers-MacBook-Pro.local>
2018-02-20 14:11:01 +0200

    testing return for testing

--- src/App.js
+++ src/App.js
@@ -16,50 +16,51 @@ class App extends Component {
   }
 
   render() {
-    const { isAuthenticated } = this.props.auth;
-
-    return (
-      <div>
-        <Navbar fluid>
-          <Navbar.Header>
-            <Navbar.Brand>
-              <a href="#">Auth0 - React</a>
-            </Navbar.Brand>
-            <Button
-              bsStyle="primary"
-              className="btn-margin"
-              onClick={this.goTo.bind(this, 'home')}
-            >
-              Home
-            </Button>
-            {
-              !isAuthenticated() && (
-                  <Button
-                    id="qsLoginBtn"
-                    bsStyle="primary"
-                    className="btn-margin"
-                    onClick={this.login.bind(this)}
-                  >
-                    Log In
-                  </Button>
-                )
-            }
-            {
-              isAuthenticated() && (
-                  <Button
-                    id="qsLogoutBtn"
-                    bsStyle="primary"
-                    className="btn-margin"
-                    onClick={this.logout.bind(this)}
-                  >
-                    Log Out
-                  </Button>
-                )
-            }
-          </Navbar.Header>
-        </Navbar>
-      </div>
-    );
+    return (<div>aaa</div>)
+  //   const { isAuthenticated } = this.props.auth;
+  //
+  //   return (
+  //     <div>
+  //       <Navbar fluid>
+  //         <Navbar.Header>
+  //           <Navbar.Brand>
+  //             <a href="#">Auth0 - React</a>
+  //           </Navbar.Brand>
+  //           <Button
+  //             bsStyle="primary"
+  //             className="btn-margin"
+  //             onClick={this.goTo.bind(this, 'home')}
+  //           >
+  //             Home
+  //           </Button>
+  //           {
+  //             !isAuthenticated() && (
+  //                 <Button
+  //                   id="qsLoginBtn"
+  //                   bsStyle="primary"
+  //                   className="btn-margin"
+  //                   onClick={this.login.bind(this)}
+  //                 >
+  //                   Log In
+  //                 </Button>
+  //               )
+  //           }
+  //           {
+  //             isAuthenticated() && (
+  //                 <Button
+  //                   id="qsLogoutBtn"
+  //                   bsStyle="primary"
+  //                   className="btn-margin"
+  //                   onClick={this.logout.bind(this)}
+  //                 >
+  //                   Log Out
+  //                 </Button>
+  //               )
+  //           }
+  //         </Navbar.Header>
+  //       </Navbar>
+  //     </div>
+  //   );
   }
 }
 
