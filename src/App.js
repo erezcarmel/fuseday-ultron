d4f1b805b413cefaa0602da10637a53a4e9e8200
Gal Noy <gal@galnoy.com>
2018-02-20 17:07:25 +0200

    new layout

--- src/App.js
+++ src/App.js
@@ -1,66 +1,178 @@
-import React, { Component } from 'react';
-import { Navbar, Button } from 'react-bootstrap';
-import './App.css';
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from 'material-ui/styles';
+import AppBar from 'material-ui/AppBar';
+import Toolbar from 'material-ui/Toolbar';
+import Typography from 'material-ui/Typography';
+import IconButton from 'material-ui/IconButton';
+import MenuIcon from 'material-ui-icons/Menu';
+import AccountCircle from 'material-ui-icons/AccountCircle';
+import Switch from 'material-ui/Switch';
+import { FormControlLabel, FormGroup } from 'material-ui/Form';
+import Menu, { MenuItem } from 'material-ui/Menu';
 
-class App extends Component {
+const styles = {
+  root: {
+    flexGrow: 1,
+  },
+  flex: {
+    flex: 1,
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20,
+  },
+};
+
+class App extends React.Component {
+  state = {
+    // auth: true,
+    anchorEl: null,
+  };
+
+  login = () => {
+    this.props.auth.login();
+  }
+
+  logout = () => {
+    this.props.auth.logout();
+  }
+
+  handleChange = (event, checked) => {
+    this.setState({ auth: checked });
+  };
+
+  handleMenu = event => {
+    this.setState({ anchorEl: event.currentTarget });
+  };
+
+  handleClose = () => {
+    this.setState({ anchorEl: null });
+  };
+
   goTo(route) {
     this.props.history.replace(`/${route}`)
   }
 
-  login() {
-    this.props.auth.login();
-  }
-
-  logout() {
-    this.props.auth.logout();
-  }
-
   render() {
-    const { isAuthenticated } = this.props.auth;
-
+    const { classes, history } = this.props;
+    const { anchorEl } = this.state;
+    const auth = this.props.auth.isAuthenticated();
+    const open = Boolean(anchorEl);
+    if (!auth) this.login()
+    if (history.location.pathname === '/') this.goTo('home')
     return (
       <div>
-        <Navbar fluid>
-          <Navbar.Header>
-            <Navbar.Brand>
-              <a href="#">Auth0 - React</a>
-            </Navbar.Brand>
-            <Button
-              bsStyle="primary"
-              className="btn-margin"
-              onClick={this.goTo.bind(this, 'home')}
-            >
-              Home
-            </Button>
-            {
-              !isAuthenticated() && (
-                  <Button
-                    id="qsLoginBtn"
-                    bsStyle="primary"
-                    className="btn-margin"
-                    onClick={this.login.bind(this)}
-                  >
-                    Log In
-                  </Button>
-                )
-            }
-            {
-              isAuthenticated() && (
-                  <Button
-                    id="qsLogoutBtn"
-                    bsStyle="primary"
-                    className="btn-margin"
-                    onClick={this.logout.bind(this)}
-                  >
-                    Log Out
-                  </Button>
-                )
-            }
-          </Navbar.Header>
-        </Navbar>
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="display3" color="inherit" >
+              Ultron Mega Uber Quiz
+            </Typography>
+            {auth && (
+              <div style={{marginLeft: 'auto'}}>
+                <IconButton
+                  aria-owns={open ? 'menu-appbar' : null}
+                  aria-haspopup="true"
+                  onClick={this.handleMenu}
+                  color="inherit"
+                >
+                  <AccountCircle style={{fontSize: '48px'}}/>
+                </IconButton>
+                <Menu
+                  id="menu-appbar"
+                  anchorEl={anchorEl}
+                  anchorOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                  }}
+                  transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                  }}
+                  open={open}
+                  onClose={this.handleClose}
+                >
+                  <MenuItem onClick={this.logout}>Logout</MenuItem>
+                </Menu>
+              </div>
+            )}
+          </Toolbar>
+        </AppBar>
       </div>
     );
   }
 }
 
-export default App;
+// MenuAppBar.propTypes = {
+//   classes: PropTypes.object.isRequired,
+// };
+
+export default withStyles(styles)(App);
+
+
+// import React, { Component } from 'react';
+// import { Navbar, Button } from 'react-bootstrap';
+// import './App.css';
+
+// class App extends Component {
+//   goTo(route) {
+//     this.props.history.replace(`/${route}`)
+//   }
+
+//   login() {
+//     this.props.auth.login();
+//   }
+
+//   logout() {
+//     this.props.auth.logout();
+//   }
+
+//   render() {
+//     const { isAuthenticated } = this.props.auth;
+
+//     return (
+//       <div>
+//         <Navbar fluid>
+//           <Navbar.Header>
+//             <Navbar.Brand>
+//               <a href="#">Auth0 - React</a>
+//             </Navbar.Brand>
+//             <Button
+//               bsStyle="primary"
+//               className="btn-margin"
+//               onClick={this.goTo.bind(this, 'home')}
+//             >
+//               Home
+//             </Button>
+//             {
+//               !isAuthenticated() && (
+//                   <Button
+//                     id="qsLoginBtn"
+//                     bsStyle="primary"
+//                     className="btn-margin"
+//                     onClick={this.login.bind(this)}
+//                   >
+//                     Log In
+//                   </Button>
+//                 )
+//             }
+//             {
+//               isAuthenticated() && (
+//                   <Button
+//                     id="qsLogoutBtn"
+//                     bsStyle="primary"
+//                     className="btn-margin"
+//                     onClick={this.logout.bind(this)}
+//                   >
+//                     Log Out
+//                   </Button>
+//                 )
+//             }
+//           </Navbar.Header>
+//         </Navbar>
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
